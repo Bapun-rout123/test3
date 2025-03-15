d0aa9625ce8ca1cfffe127131c6613722eab0157
Bapun-rout123 <rout6937@gmail.com>
2025-03-15 16:23:44 +0530

    Update Test3Application.java

--- src/main/java/com/test3/Test3Application.java
+++ src/main/java/com/test3/Test3Application.java
@@ -9,9 +9,10 @@ public class Test3Application {
 	public static void main(String[] args) {
 		String name = "mike";
 		int x = 100;
-		int y = 200;
-		double z = 10.3;
 		boolean isPresent= false;
+		int y = 200;
+		char x = 'a';
+		
 		
 		SpringApplication.run(Test3Application.class, args);
 	}
